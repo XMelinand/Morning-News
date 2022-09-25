e1fd00978e2628bff577246ae9f2446c02050075
XMelinand <107178759+XMelinand@users.noreply.github.com>
2022-09-25 04:11:55 -0400

    secure

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
+import { DB_KEY } from '@env'
 
 var options = {
     connectTimeoutMS: 5000,
     useNewUrlParser: true,
     useUnifiedTopology : true
     }
-    mongoose.connect('mongodb+srv://admin:ywvpDrOboNxWBZY5@clusterorder66.sgrgl.mongodb.net/morningnews?retryWrites=true&w=majority',
+    mongoose.connect(`mongodb+srv://admin:${DB_KEY}.sgrgl.mongodb.net/morningnews?retryWrites=true&w=majority`,
         options,        
         function(err) { if (err){
         console.log(err);
